01f2bcf54b139cafa8d3c882fff1e19009612ef3
牟江湖 <1441901570@qq.com>
2021-08-11 09:52:53 +0800

    集成fs

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {SafeAreaView, ScrollView, Text, View, TouchableOpacity} from "react-native";
 import { Button, Icon } from '@ant-design/react-native';
+import fs from "react-native-fs"
 
 const Home = (props: { navigation: any }) => {
     const userInfo = useSelector(state => state.userInfo);
-
+    const openFile = () => {
+        console.log(fs.DocumentDirectoryPath);
+        console.log(fs.DownloadDirectoryPath);
+    }
 
     return <SafeAreaView>
         <ScrollView>
@@ -14,8 +18,8 @@ const Home = (props: { navigation: any }) => {
                     {userInfo.username}
 
                 </Text>
-                <Icon name="account-book" size="md" color="white" />
-                <Button>antd-mobile button</Button>
+                <Icon name="alibaba" size="md" color="white" />
+                <Button onPress={openFile}>antd-mobile button</Button>
             </View>
             <View style={{height: 300, backgroundColor: 'red'}}>
                 <Text>
